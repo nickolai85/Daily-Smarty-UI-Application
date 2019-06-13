13713e5f1aa9df710fbc2eca1ebaeb062d4ffc4c
Nicolai <organ.nicolai@gmail.com>
2019-06-13 09:10:35 +0300

    Creating Searchbar and Recent Posts Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Logo from './logo';
-
+import SearchBar from './searchBar';
+import RecentPosts from './recentPosts';
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <Logo/>
-        <h2>React Redux Router</h2>
+        <SearchBar/>
+          <RecentPosts/>
       </div>
     );
   }
