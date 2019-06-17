17a2712b4bc36a0fb1d082ae0c5b00e0a1f67a14
Nicolai <organ.nicolai@gmail.com>
2019-06-17 09:28:43 +0300

    setup redux devtools and fixed reducer for set_recent_posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
-
+import posts from './postsReducer';
 const rootReducer = combineReducers({
-  form
+  form,
+  posts
 });
 
 export default rootReducer;
