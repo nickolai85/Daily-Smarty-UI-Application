8f146f6cc0d794766f4d429b332a12a77a5d7066
Nicolai <organ.nicolai@gmail.com>
2019-06-18 22:50:32 +0300

    began styling the home component and talked about CSS Grid in codepen

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class Home extends Component {
-  
+
   handleSearchBarSubmit(query) {
     console.log('trying to handle submit for query', query);
     this.props.fetchPostsWithQuery(query, () => {
@@ -17,10 +17,10 @@ class Home extends Component {
 
   render() {
     return (
-      <div className='app'>
+      <div className="home">
         <Logo/>
         <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
-          <RecentPosts/>
+        <RecentPosts/>
       </div>
     );
   }
