838cde79055605a16326b02eeb5a6fa495d7537a
Nicolai <organ.nicolai@gmail.com>
2019-06-18 09:07:48 +0300

    added SET_RESULTS_POSTS reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { SET_RECENT_POSTS } from './types';
+import { SET_RECENT_POSTS, SET_RESULTS_POSTS } from './types';
 import axios from 'axios'
 export function fetchRecentPosts() {
   return function(dispatch) {
@@ -8,7 +8,7 @@ export function fetchRecentPosts() {
     .then(response => {
         console.log(response.data.posts);
         dispatch({
-            type: SET_RECENT_POSTS,
+            type: SET_RESULTS_POSTS,
             payload: response.data.posts
         })
     })
@@ -20,10 +20,10 @@ export function fetchPostsWithQuery(query) {
       axios.get(`https://api.dailysmarty.com/search?q=${query}`)
           .then(response => {
               console.log(response.data.posts);
-              // dispatch({
-              //     type: SET_RECENT_POSTS,
-              //     payload: response.data.posts
-              // })
+               dispatch({
+                   type: SET_RECENT_POSTS,
+                   payload: response.data.posts
+               })
           })
   }
 }
